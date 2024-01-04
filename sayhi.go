ddf6f695d0f36cf12f4f82f4b8c111abdb4e6257
aberrant00 <robert.tinca@mail.com>
2024-01-04 11:53:34 +0100

    whoop

--- sayhi.go
+++ sayhi.go
@@ -3,7 +3,7 @@ package sayhi
 import "fmt"
 
 func Sayhi(name string) string {
-	message := fmt.Sprintf("Wel whake hehehehl well well if it isnt heh, %v!", name)
+	message := fmt.Sprintf("Wel whake whooooop well well if it isnt heh, %v!", name)
 
 	return message
 }
