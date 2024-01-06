1a0f39d92dd7cc4dbb790123769c4ed966b63aa1
aberrant00 <robert.tinca@mail.com>
2024-01-06 21:14:53 +0100

    ggy

--- sayhi.go
+++ sayhi.go
@@ -3,7 +3,7 @@ package sayhi
 import "fmt"
 
 func Sayhi(name string) string {
-	message := fmt.Sprintf("Welcome fag, %v!", name)
+	message := fmt.Sprintf("Welcome faggy, %v!", name)
 
 	return message
 }
